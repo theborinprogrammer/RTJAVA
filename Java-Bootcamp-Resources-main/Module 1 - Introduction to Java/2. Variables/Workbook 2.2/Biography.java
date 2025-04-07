c8faee37807760191b5c79964f27a6626f092bad
Sourov Saha <redrider@Sourovs-MacBook-Air.local>
2025-04-07 16:19:54 +0600

    Day 2 half day grind

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -3,20 +3,20 @@ public class Biography {
         
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
 
+        String name = "Sourov Kumar Saha Shetu";
+        int age = 19;
+        String country = "Bangladesh";
+        String sport = "FootBall";
+        int hours = 3;
+        String game = "FC24";
+        String subject = "Math";
+        char grade = 'A';
+        System.out.println(" ");
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day.");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored a " + grade + ".");
 
-        // name (text)
-        // age (whole number)
-        // country (text)
-        // sport (text)
-        // hours (whole number)
-        // game (text)
-        // subject (text)
-        // grade (character)
-     
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
 
         // Compare your result to what's on Learn the Part.
     }
--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
@@ -3,23 +3,33 @@ public class AppleStore {
         int numOfApples = 0;
         int numOfCustomers = 0;
         double profit = 0;
+        int sales = 0;
+        double pricePerApple = .40;
 
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.3 to access the link).
-
+        System.out.println("");
         System.out.println("You picked 500 apples from an apple orchard");
-        
+        numOfApples += 500;
         System.out.println("Time for business! You're selling each apple for 40 cents");
-
         System.out.println("One customer walked in. He bought 4 apples!");
-
+        sales = 4;
+        numOfApples -=4;
+        profit += sales*pricePerApple;
+        numOfCustomers++;
         System.out.println("Another customer walked in. He bought 20 apples!");
-
+        sales = 20;
+        numOfApples -= 20;
+        profit += sales*pricePerApple;
+        numOfCustomers++;
         System.out.println("Another customer walked in. She bought 200 apples!");
-        
+        sales = 200;
+        numOfApples -= 200;
+        profit += sales*pricePerApple;
+        numOfCustomers++;
 
-        System.out.println("Wow! So far, you made: $<profit>");
-        System.out.println("<number of customers> + customers love your apples.");
-        System.out.println("You have <number of apples> apples left.");
+        System.out.println("Wow! So far, you made: " + profit);
+        System.out.println(numOfCustomers +" customers love your apples.");
+        System.out.println("You have "+ numOfApples +" apples left." + " We will sell more tommorow");
 
         // Compare your result to what's on Learn the Part.  
         
